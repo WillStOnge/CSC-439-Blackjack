a839b7b834f1d9d8ca2cf0830a60acfb726ddb15
Justin Gallagher <gallagherj3@nku.edu>
2021-02-22 20:36:29 -0500

    made changes to the Number and Suit class, giving them toString methods

--- src/csc439team1/blackjack/model/Number.java
+++ src/csc439team1/blackjack/model/Number.java
@@ -3,7 +3,7 @@ package csc439team1.blackjack.model;
 /**
  * The Number enum represents the 13 types of playing card 'numbers'.
  * Note the ordering of the card numbers:
- * 0(ACE), 1(TWO), 2(THREE), ... , 12(KING)
+ * ACE, TWO, THREE, ... , KING
  *
  * @author Justin Gallagher
  * @version 0.1
@@ -22,5 +22,58 @@ public enum Number
 	TEN,
 	JACK,
 	QUEEN,
-	KING
+	KING;
+
+	/**
+	 * Overridden toString() method for the Number class that prints out the string representation an
+	 * individual playing card's associated 'number'.
+	 *
+	 * @return the string representation of the playing card's 'number'
+	 */
+	@Override
+	public String toString() {
+		String numString = "";
+		switch (this) {
+			case ACE:
+				numString = "A";
+				break;
+			case TWO:
+				numString = "2";
+				break;
+			case THREE:
+				numString = "3";
+				break;
+			case FOUR:
+				numString = "4";
+				break;
+			case FIVE:
+				numString = "5";
+				break;
+			case SIX:
+				numString = "6";
+				break;
+			case SEVEN:
+				numString = "7";
+				break;
+			case EIGHT:
+				numString = "8";
+				break;
+			case NINE:
+				numString = "9";
+				break;
+			case TEN:
+				numString = "10";
+				break;
+			case JACK:
+				numString = "J";
+				break;
+			case QUEEN:
+				numString = "Q";
+				break;
+			case KING:
+				numString = "K";
+				break;
+		}
+		return numString;
+	}
 }
--- src/csc439team1/blackjack/model/Suit.java
+++ src/csc439team1/blackjack/model/Suit.java
@@ -11,5 +11,31 @@ public enum Suit
 	HEARTS,
 	SPADES,
 	DIAMONDS,
-	CLUBS
+	CLUBS;
+
+	/**
+	 * Overridden toString() method for the Suit class that prints out the string representation of the
+	 * suit of an individual playing card in all uppercase.
+	 *
+	 * @return the string representation of the playing card's suit
+	 */
+	@Override
+	public String toString() {
+		String suitString = "";
+		switch (this) {
+			case HEARTS:
+				suitString = "HEARTS";
+				break;
+			case SPADES:
+				suitString = "SPADES";
+				break;
+			case DIAMONDS:
+				suitString = "DIAMONDS";
+				break;
+			case CLUBS:
+				suitString = "CLUBS";
+				break;
+		}
+		return suitString;
+	}
 }
