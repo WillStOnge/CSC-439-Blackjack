b4afcfa4e5b5f8f86ec5a82e5ea09a0b1830b460
as2022 <77690014+as2022@users.noreply.github.com>
2021-03-14 18:17:45 -0400

    Update Deck.java
    IllegalStateException
    Random instead of Math.random()

--- src/csc439team1/blackjack/model/Deck.java
+++ src/csc439team1/blackjack/model/Deck.java
@@ -1,29 +1,28 @@
 package csc439team1.blackjack.model;
 
-import java.rmi.NoSuchObjectException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
- * The Deck class is used to create an instance that represents a
- * deck of playing cards and includes the following properties:
- *
- * numCards is the number of cards remaining in the deck
- * deck is the data structure that represents the deck
- *
- * @author Alex Smithson
- * @version 0.1
- */
-public class Deck {
+     * The Deck class is used to create an instance that represents a
+     * deck of playing cards and includes the following properties:
+     *
+     * deck is of type List and represents the deck
+     *
+     * @author Alex Smithson
+     * @version 0.2
+     */
+    public class Deck {
 
-    private List<Card> deck;
+    private final List<Card> deck;
 
     /**
      * The Deck class constructor creates the List that represents the deck
      * and populates it with 52 unique Card instances
      */
     public Deck(){
-        deck = new ArrayList<Card>();
+        deck = new ArrayList<>();
 
         for(Number curNumber : Number.values())
             for (Suit curSuit : Suit.values())
@@ -35,15 +34,16 @@ public class Deck {
      * and removes it from the List, deck.
      *
      * @return a randomly selected card
-     * @throws NoSuchObjectException if there are no cards left in the deck.
+     * @throws IllegalStateException if there are no cards left in the deck.
      */
-    public Card pick() throws NoSuchObjectException{
+    public Card pick() {
         Card returnCard;
+        Random random = new Random();
 
         if(deck.size() == 0)
-            throw new NoSuchObjectException("The deck is empty");
+            throw new IllegalStateException("The deck is empty");
 
-        returnCard = deck.get((int) Math.random() * deck.size());
+        returnCard = deck.get(random.nextInt() * deck.size());
         deck.remove(returnCard);
 
         return returnCard;
