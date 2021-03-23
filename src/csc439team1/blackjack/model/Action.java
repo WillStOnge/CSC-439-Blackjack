c9721b301f5c724079ecd8556b4645fac48c9a10
Will St. Onge <ongewill@gmail.com>
2021-03-23 00:14:40 -0400

    Added Action enum

---
+++ src/csc439team1/blackjack/model/Action.java
@@ -0,0 +1,37 @@
+package csc439team1.blackjack.model;
+
+/**
+ * Represents the actions which can be taken under certain conditions determined by the controller.
+ *
+ * @author Will St. Onge
+ * @version 0.3
+ */
+public enum Action
+{
+	HIT("hit"),
+	STAND("stand"),
+	DOUBLE("double");
+
+	private final String name;
+
+	/**
+	 * Action enum constructor that sets the name of the action.
+	 *
+	 * @param name Name of the action
+	 */
+	Action(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * Overridden toString() method for the Action enum that returns the string representation of an action.
+	 *
+	 * @return the string representation of an action.
+	 */
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+}
