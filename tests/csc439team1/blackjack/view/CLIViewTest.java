c6f1ef68aa0d6c2bc788a507111c31174800b78b
Will St. Onge <ongewill@gmail.com>
2021-03-31 16:52:09 -0400

    Cunningham said we dont need to unit test the views or main class.

--- tests/csc439team1/blackjack/view/CLIViewTest.java
+++
@@ -1,9 +0,0 @@
-package csc439team1.blackjack.view;
-
-import org.junit.Test;
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.CoreMatchers.*;
-
-public class CLIViewTest
-{
-}
